0bf97d35842d198c7b1f43571c7b7e1245fc3ac0
Jonatas Souza <jonatas.souza@2amigos.us>
2024-10-24 10:45:50 -0300

    actions v1

--- packages/crudx/jest.config.ts
+++ packages/crudx/jest.config.ts
@@ -7,5 +7,6 @@ export default {
     "^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }],
   },
   moduleFileExtensions: ["ts", "js", "html"],
+  coverageReporters: ["json", "clover"],
   coverageDirectory: "../../coverage/packages/crudx",
 };
