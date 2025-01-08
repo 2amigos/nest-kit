991a711ecb9d2b4a45ad2822559173a7de453bfe
Jonatas Souza <jonatas.souza@2amigos.us>
2025-01-08 00:26:17 -0300

    tests should not run in parallel

--- test-all.sh
+++ test-all.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 docker compose exec -it 2am-nestkit /bin/sh -c "yarn nx reset"
-docker compose exec -it 2am-nestkit /bin/sh -c "yarn nx run-many -t test --parallel=false"
+docker compose exec -it 2am-nestkit /bin/sh -c "yarn nx run crudx:test"
+docker compose exec -it 2am-nestkit /bin/sh -c "yarn nx reset"
+docker compose exec -it 2am-nestkit /bin/sh -c "yarn nx run crudx-swagger:test"
