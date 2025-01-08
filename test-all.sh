bfe20d2d187cca33e59ad23308090e850c1df225
Jonatas Souza <jonatas.souza@2amigos.us>
2025-01-08 00:18:52 -0300

    tests should not run in parallel

--- test-all.sh
+++ test-all.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker compose exec -it 2am-nestkit /bin/sh -c "yarn nx reset"
-docker compose exec -it 2am-nestkit /bin/sh -c "yarn test"
+docker compose exec -it 2am-nestkit /bin/sh -c "yarn nx run-many -t test --parallel=false"
