8547930471f06a940e65923781c019eb26a7607a
Aleksandar Panic <arekusanda1@gmail.com>
2025-12-05 19:54:53 +0100

    try fix tests

--- test-all.sh
+++ test-all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-docker compose exec -it 2am-nestkit /bin/sh -c "yarn nx reset"
-docker compose exec -it 2am-nestkit /bin/sh -c "yarn nx run crudx:test"
-docker compose exec -it 2am-nestkit /bin/sh -c "yarn nx reset"
-docker compose exec -it 2am-nestkit /bin/sh -c "yarn nx run crudx-swagger:test"
+yarn nx reset
+yarn nx run crudx:test
+yarn nx reset
+yarn nx run crudx-swagger:test
