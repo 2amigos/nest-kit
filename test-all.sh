14007ec39fafeebb4665976e8a2abb58d4c405d6
Aleksandar Panic <arekusanda1@gmail.com>
2025-12-05 19:45:44 +0100

    fix tests

--- test-all.sh
+++ test-all.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 docker compose exec -it 2am-nestkit /bin/sh -c "yarn nx reset"
 docker compose exec -it 2am-nestkit /bin/sh -c "yarn nx run crudx:test"
 docker compose exec -it 2am-nestkit /bin/sh -c "yarn nx reset"
